97710643b46aa51629b8f1f753d5ab28a1cd8941
Tomson <tomson.martin@innovaturelabs.com>
2024-03-07 09:56:43 +0530

    home and footer implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,22 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/lib/esm/**/*.js'
+    'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   theme: {
     extend: {
-      colors:{
-        'neutralSilver':"#F5F7FA",
-        'neutralDGrey':"#4D4D4D",
-        "brandPrimary":"#4CAF4F",
-        "neutralGrey":"#89939E"
-      }
+colors : {
+  'neutralSilver' : '#F5F7FA',
+  'NeutralDGrey' : '#4D4D4D',
+  'BrandPrimary' : '#4CAF4F',
+  'NeutralWhite' : '#FFFFFF',
+  'grey900' : '#18191F',
+}
+
     },
   },
   plugins: [
-    require('flowbite/plugin')
+    // eslint-disable-next-line no-undef
+    require('flowbite/plugin'),
   ],
 }
